a8996ffae3cba4521d9bd9a7b6c0846988ff2242
Stevenz <kikexgaz@gmail.com>
2023-05-04 09:36:55 -0300

    Up

---
+++ src/data.js
@@ -0,0 +1,46 @@
+
+export const data = [
+	{
+		id: 1,
+		img: 'https://i.imgur.com/OSDdw59.jpg',
+		nameProduct: 'Zapatos Nike',
+		price: 80,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://i.imgur.com/tS6ic1f.jpg',
+		nameProduct: 'Razer Kraken X',
+		price: 60,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://i.imgur.com/AQXjyqJ.jpg',
+		nameProduct: 'Smartwatch',
+		price: 90,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'https://i.imgur.com/D2yIpAZ.jpg',
+		nameProduct: 'Perfume',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'https://i.imgur.com/y6G3MCl.jpg',
+		nameProduct: 'Mochila',
+		price: 40,
+		quantity: 1,
+	},
+	{
+		id: 6,
+		img:'https://i.imgur.com/pajkyEN.jpg',
+		nameProduct: 'Psn Card',
+		price: 10,
+		quantity: 1,
+	},
+	// Aquí pueden agregar más o modificar los productos existentes
+];
